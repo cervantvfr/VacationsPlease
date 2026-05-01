12ba7c04b3cbcb6d54a1df941905b04b804885fa
vic <cervantvfr@gmail.com>
2026-05-01 10:41:38 +0200

    Initial commit

---
+++ src/main/java/dev/cervantvfr/vacationsplease/VacationsPleaseApplication.java
@@ -0,0 +1,13 @@
+package dev.cervantvfr.vacationsplease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationsPleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationsPleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cervantvfr/vacationsplease/VacationsPleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cervantvfr.vacationsplease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationsPleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
